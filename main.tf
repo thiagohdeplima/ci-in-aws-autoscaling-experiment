84203e775729ef10c7692f995f4d7a9b46f6b47d
Thiago Lima <thiago.lima@brainiac.home>
2020-04-08 21:38:37 -0300

    chore: Add main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,25 @@
+terraform {
+  backend "s3" {
+    region = "us-east-2"
+    key    = "terraform/state"
+    bucket = "codebrain-terraform"
+  }
+  
+  required_providers {
+    aws = ">= 2.14.0"
+  }
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-east-2"
+}
+
+provider "azurerm" {
+  version = ">= 2.0.0"
+  features {}
+}
+
+provider "google" {
+
 }
