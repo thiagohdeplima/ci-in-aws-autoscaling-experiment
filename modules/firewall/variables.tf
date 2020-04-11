30befd0009eb1f6616872ca4e89fae47d6c82dd6
Thiago Lima <thiago.lima@brainiac.home>
2020-04-11 18:52:13 -0300

    feat: Add NFS security group

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -16,6 +16,20 @@ variable "environment" {
 variable "groups" {
   type    = "map"
   default = {
+    nfs = {
+      ingress = {
+        allowed_port  = 2049
+        allowed_proto = "tcp"
+        allowed_cidrs = [
+          "10.0.0.0/26",
+          "10.0.0.64/26",
+          "10.0.0.128/26",
+          "10.0.0.192/26"
+        ]
+        allowed_secgrps = []
+      }
+    }
+
     ssh = {
       ingress = {
         allowed_port    = 22
